353e5ecfb08a1392a7ce3c1223c9075b7ca8f3d9
GrEEman <mmesaros93@gmail.com>
2014-11-06 17:03:11 +0100

    Basic skinning shader v2.2
    update basic skinning shader

--- Basic skinning shader/basic_skinning_vt.glsl
+++ Basic skinning shader/basic_skinning_vt.glsl
@@ -1,19 +1,17 @@
 #version 400
 
-#define NUMBER_OF_BONES 100  // this have to be changed to actual number of bones of worm
-
 layout(location = 0) in vec4 vertex_position;
-layout(location = 1) in vec4 index_of_bone;
+layout(location = 1) in ivec4 index_of_bone;
 layout(location = 2) in vec4 weight;
 
 uniform mat4 projection, view, model;
-uniform mat4 bones[number_of_bones];
+uniform mat4 bones[#NUMBER_OF_BONES#];
 
 void main() {
   vec4 transformed_vertex = vec4(0.0, 0.0, 0.0, 0.0); 
-  transformed_vertex += weight.x * (vertex_position * bones[index_of_bone.x]);
-  transformed_vertex += weight.y * (vertex_position * bones[index_of_bone.y]);
-  transformed_vertex += weight.z * (vertex_position * bones[index_of_bone.z]);
-  transformed_vertex += weight.w * (vertex_position * bones[index_of_bone.w]);
+  transformed_vertex += weight.x * (bones[index_of_bone.x] * vertex_position);
+  transformed_vertex += weight.y * (bones[index_of_bone.y] * vertex_position);
+  transformed_vertex += weight.z * (bones[index_of_bone.z] * vertex_position);
+  transformed_vertex += weight.w * (bones[index_of_bone.w] * vertex_position);
   gl_Position = projection * view * model * vec4(transformed_vertex.x, transformed_vertex.y, transformed_vertex.z, 1.0);
 }
